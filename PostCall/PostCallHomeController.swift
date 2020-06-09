6ebf7475023201ed0f817680815d4d8958e19223
jesloam <jesloa@gmail.com>
2020-06-09 12:57:51 +0530

    postcall project created

---
+++ PostCall/PostCallHomeController.swift
@@ -0,0 +1,20 @@
+//
+//  PostCallHomeController.swift
+//  PostCall
+//
+//  Created by Joe on 09/06/20.
+//  Copyright © 2020 jess. All rights reserved.
+//
+
+import UIKit
+
+class PostCallHomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
---
+++ PostCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PostCall
+//
+//  Created by Joe on 09/06/20.
+//  Copyright © 2020 jess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
